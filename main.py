5e5cc1ad63d02dfc5e687490ac7ba119b759808f
Anuj Singh <kumaranuj1303@gmail.com>
2022-01-02 23:39:56 +0530

    filename changed again

--- main.py
+++ main.py
@@ -1,64 +1,14 @@
-# from fastapi import FastAPI
-#
-# app = FastAPI()
-#
-# # Just a comment
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
+from fastapi import FastAPI
 
-
-# A Bare Bones Slack API
-# Illustrates basic usage of FastAPI
-from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
-from typing import List
-
-# Message class defined in Pydantic
-class Message(BaseModel):
-    channel: str
-    author: str
-    text: str
-
-
-# Instantiate the FastAPI
 app = FastAPI()
 
-# In a real app, we would have a database.
-# But, let's keep it super simple for now!
-channel_list = ["general", "dev", "marketing"]
-message_map = {}
-for channel in channel_list:
-    message_map[channel] = []
-
-@app.get("/status")
-def get_status():
-    """Get status of messaging server."""
-    return ({"status":  "running"})
-
-@app.get("/channels", response_model=List[str])
-def get_channels():
-    """Get all channels in list form."""
-    return channel_list
+# Just a comment
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-@app.get("/messages/{channel}", response_model=List[Message])
-def get_messages(channel: str):
-    """Get all messages for the specified channel."""
-    return message_map.get(channel)
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
 
-
-@app.post("/post_message", status_code=status.HTTP_201_CREATED)
-def post_message(message: Message):
-    """Post a new message to the specified channel."""
-    channel = message.channel
-    if channel in channel_list:
-        message_map[channel].append(message)
-        return message
-    else:
-        raise HTTPException(status_code=404, detail="channel not found")
