4f11a5cb17fb280b821447f7dadfeea749f2eb2f
Anuj Singh <kumaranuj1303@gmail.com>
2022-01-02 23:59:46 +0530

    war begins

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
+from handler import Email
 
 app = FastAPI()
+
 
 # Just a comment
 @app.get("/")
 async def root():
+    mymail = Email()
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
