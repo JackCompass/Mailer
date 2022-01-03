3dbcc89fef5f2e7b9a458905b1fa75a49e38ce01
Anuj Singh <kumaranuj1303@gmail.com>
2022-01-03 10:55:22 +0530

    Production level change

--- mailhandler.py
+++ mailhandler.py
@@ -22,7 +22,7 @@ class Email:
                 self.creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'xcredentials.json', self.scope
+                    'credentials.json', self.scope
                 )
                 self.creds = flow.run_local_server(port=0)
 
