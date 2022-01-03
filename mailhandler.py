aaeac0b497337643e1a07f0112d0e247f9beabcc
Anuj Singh <kumaranuj1303@gmail.com>
2022-01-03 15:19:50 +0530

    war 5

--- mailhandler.py
+++ mailhandler.py
@@ -1,47 +1,47 @@
-from __future__ import print_function
-from os import path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-import google_auth_oauthlib.flow
-from starlette.responses import RedirectResponse
-
-
-class Email:
-    def __init__(self):
-        self.scope = 'https://mail.google.com/'
-        self.creds = None
-        self.setup()
-
-    def setup(self):
-        """This function is going to do the basic setup for the email object"""
-        if path.exists('token.json'):
-            # If the path exists then we just use the token.json file and use to verify credentials
-            self.creds = Credentials.from_authorized_user_file('token.json', self.scope)
-
-        if not self.creds or not self.creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
-            else:
-                flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-                    'credentials.json', self.scope
-                )
-                flow.redirect_uri = 'https://mailcleaner.herokuapp.com/authenticated/'
-                authorization_url, state = flow.authorization_url(
-                    access_type='offline',
-                    include_granted_scopes='true')
-                return RedirectResponse(authorization_url)
-
-            # with open('token.json', 'w') as token:
-            #     token.write(self.creds.to_json())
-
-    def fetch_email(self):
-        # TODO: write a function which is going to fetch a number of emails
-        pass
-
-    def search_email(self):
-        # TODO: Write a function which is going to search and return emails with the search keyword
-        pass
-
-    def trash_email(self):
-        # TODO: Write a function which move selected emails to trash
-        pass
+# from __future__ import print_function
+# from os import path
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# import google_auth_oauthlib.flow
+# from starlette.responses import RedirectResponse
+#
+#
+# class Email:
+#     def __init__(self):
+#         self.scope = 'https://mail.google.com/'
+#         self.creds = None
+#         self.setup()
+#
+#     def setup(self):
+#         """This function is going to do the basic setup for the email object"""
+#         if path.exists('token.json'):
+#             # If the path exists then we just use the token.json file and use to verify credentials
+#             self.creds = Credentials.from_authorized_user_file('token.json', self.scope)
+#
+#         if not self.creds or not self.creds.valid:
+#             if self.creds and self.creds.expired and self.creds.refresh_token:
+#                 self.creds.refresh(Request())
+#             else:
+#                 flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+#                     'credentials.json', self.scope
+#                 )
+#                 flow.redirect_uri = 'https://mailcleaner.herokuapp.com/authenticated/'
+#                 authorization_url, state = flow.authorization_url(
+#                     access_type='offline',
+#                     include_granted_scopes='true')
+#                 return RedirectResponse(authorization_url)
+#
+#             # with open('token.json', 'w') as token:
+#             #     token.write(self.creds.to_json())
+#
+#     def fetch_email(self):
+#         # TODO: write a function which is going to fetch a number of emails
+#         pass
+#
+#     def search_email(self):
+#         # TODO: Write a function which is going to search and return emails with the search keyword
+#         pass
+#
+#     def trash_email(self):
+#         # TODO: Write a function which move selected emails to trash
+#         pass
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, Request, Response
 from starlette.responses import RedirectResponse
-from mailhandler import Email
+from __future__ import print_function
+from os import path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import google_auth_oauthlib.flow
+import os
 
 app = FastAPI()
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://mail.google.com/']
 
 
 # Just a comment
@@ -18,7 +26,28 @@ async def say_hello(name: str):
 
 @app.get('/verify')
 def verify_account():
-    return Email()
+    """Shows basic usage of the Gmail API.
+        Lists the user's Gmail labels.
+        """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            flow.redirect_uri = 'https://mailcleaner.herokuapp.com/authenticated/'
+            authorization_url, state = flow.authorization_url(access_type='offline', include_granted_scopes='true')
+            return RedirectResponse(authorization_url)
+    else:
+        return {'message': 'failure again'}
 
 
 @app.get('/authencticated/')
